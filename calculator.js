f901fcbd37582056d583336d2ff660da22f07dc5
tailun828 <tailun828@gmail.com>
2014-04-10 10:22:05 -0700

    4/10

--- calculator.js
+++ calculator.js
@@ -7,19 +7,19 @@ var calculate = function(input) {
 	var shortLetterArray = letterArray.filter(function(e){return e});
 
 	/*special case with exponential function*/
-
-    /*adding paranthesis to each number to solve "sqrt of 6"*/
 	for (var i =0; i<numArray.length; i++) {
 			if (numArray[i] !== '' ) {
 				numArrayPara = '(' + numArray[i] + ')';            /*add paranthesis around the naked number*/
 				input = input.replace(numArray[i],numArrayPara);                  /*replaced naked number with clothed number*/
 			};
 		};
+    /*adding paranthesis to each number to solve "sqrt of 6"*/
+
 
     /* Match and replace operator */
 	var listofOperations = ['squarerootof','mutliply' ,'times','add', 'and','plus','divideover','minus','subtract','sineof','tangentof','cosineof','exp', 'e^']; /*fix minussubtract wrong match*/
 	var operation = [ 'Math.sqrt', '*','*', '+','+','+', '/', '-','-', 'Math.sin', 'Math.tan', 'Math.cos','Math.exp', 'Math.exp'];
-	var operationLatex = [ "\\sqrt", '*','*', '+','+','+', '/', '-','-', '\\sin', '\\tan', '\\cos','\\exp', '\\exp'];
+	var operationLatex = [ "\\sqrt", '*','*', '+','+','+', '\\over', '-','-', '\\sin', '\\tan', '\\cos','\\exp', '\\exp'];
 	var scoreArray = [];
 	for (var i = 0; i<shortLetterArray.length; i++) {    /*loop through all the unrefined operators*/
 		var unrefineOperatorString = shortLetterArray[i];
@@ -36,10 +36,10 @@ var calculate = function(input) {
 				}
 			}
 		scoreArray[j] = score;
-		}
+		}	
 		var maxIndex = scoreArray.indexOf(Math.max.apply(Math, scoreArray));  /*Find which operator fits best!!*/  
 		input = input.replace(shortLetterArray[i], operation[maxIndex]);         /*replace unrefine with refine operator in input string*/ 
-		};
+	};
 
 	var ansArray = [input,eval(input)];
 	return (ansArray);	
