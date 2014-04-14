61cb945684c1ba2b7c8c1c3963ba50bdc2b8c4c6
tailun828 <tailun828@gmail.com>
2014-04-13 21:58:30 -0700

    4/13

--- chemToolParser.js
+++ chemToolParser.js
@@ -17,14 +17,13 @@ var smilesParser = function(data) {
         var chemFormula = data.substring(0,data.indexOf('APtclcactv'));  // get chem formula by taking substring 0 to wherre APtclc starts
         data = data.split(" ");   // made array of the data
         data = data.filter(function(e){return e});  //got rid of empty cells in the array
-        var numAtom = data[5];
-        var numBond = data[6];
+        var numAtom = Number(data[5]);
+        var numBond = Number(data[6]);
         var molObject = new Object();
         var bondPath = new Object();
         var counter = 0; //counter for xy coordinate (16 apart)
         var counter2 = 0; //counter for bond path (7 apart)
-        var bondPathData = data.slice(16*(Number(numAtom)+1) , data.length-8)   //shrunk the data to only the bond path
-
+        var bondPathData = data.slice(16*(Number(numAtom)+1) , data.length)   //shrunk the data to only the bond path
 
         function newElement(atomNumber , xCoordinate, yCoordinate, elementType, stereochemistry) {
                 this.atomNumber = atomNumber;
@@ -43,19 +42,21 @@ var smilesParser = function(data) {
         }
 
 
-        for (var i = 0; i < numAtom ; i++) {
-            molObject[i] = new newElement(i, (data[16 + counter]) , data[17 + counter], Number(data[19 + counter]), Number(data[22 + counter]));
+        for (var i = 1; i < numAtom + 1 ; i++) {
+            molObject[i] = new newElement(i, Number(data[16 + counter]) , Number(data[17 + counter]), (data[19 + counter]), Number(data[22 + counter]));
             counter = counter + 16;
         };
 
-        for (var i = 0; i < numBond; i++) {
+        for (var i = 1; i < numBond + 1; i++) {
             bondPath[i] = new newBond(bondPathData[0+counter2], bondPathData[1+counter2], bondPathData[2 +counter2]);
             counter2 = counter2+7;
+            /*document.write(bondPath[i].origin, bondPath[i].end, '\s')*/
         }
 
-        for (var i = 0; i <numBond-1; i++) {
-            bondPath[i].originCoordinate = [Number(molObject[Number(bondPath[i].origin)].xCoordinate), Number(molObject[Number(bondPath[i].origin)].yCoordinate)];
-            bondPath[i].endCoordinate = [Number(molObject[Number(bondPath[i].end)].xCoordinate), Number(molObject[Number(bondPath[i].end)].yCoordinate)];
+        for (var i = 1; i <numBond+1; i++) {
+            bondPath[i].originCoordinate = [molObject[Number(bondPath[i].origin)].xCoordinate,molObject[Number(bondPath[i].origin)].yCoordinate];
+            bondPath[i].endCoordinate = [molObject[Number(bondPath[i].end)].xCoordinate, molObject[Number(bondPath[i].end)].yCoordinate];
+            /*document.write(bondPath[i].origin);*/
         };
 
 
